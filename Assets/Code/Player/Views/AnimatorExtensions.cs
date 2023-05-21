32a2f517ebea8a8d827e69f4b9499e08517156cf
Carla Florencia Sosa Padilla <carla_sp88@hotmail.com>
2023-05-21 18:45:30 -0300

    Clean up AnimatorExtensions.cs

--- Assets/Code/Player/Views/AnimatorExtensions.cs
+++ Assets/Code/Player/Views/AnimatorExtensions.cs
@@ -7,20 +7,8 @@ using UnityEngine;
 
 public static class AnimatorExtensions
 {
-    public static IObservable<Unit> WaitUntilAnimationIsPlayedObservable(this Animator animator, int state,
-        int layerIndex = 0)
-    {
-        return animator.WaitUntilAnimationIsPlayed(state, layerIndex).ToObservable();
-    }
-
-    private static IEnumerator WaitUntilAnimationIsPlayed(this Animator animator, int state, int layerIndex)
-    {
-        return animator.WaitUntilAnimationIsPlayed(new[] { state }, layerIndex);
-    }
-
     private static IEnumerator WaitUntilAnimationIsPlayed(this Animator animator, int[] states, int layerIndex)
     {
-        //Wait at least for one animator update so it triggers any pending transition;
         yield return null;
 
         while (
@@ -36,18 +24,6 @@ public static class AnimatorExtensions
         int layerIndex = 0, float normalizedTimeOffset = 0f)
     {
         return animator.WaitUntilCurrentAnimationEnds(layerIndex, normalizedTimeOffset).ToObservable();
-    }
-
-    public static IObservable<Unit> WaitUntilAnimationEndsAsObservable(this Animator animator, int[] states,
-        int layerIndex = 0, float normalizedTimeOffset = 0f)
-    {
-        return animator.WaitUntilAnimationEnds(states, layerIndex, normalizedTimeOffset).ToObservable();
-    }
-
-    public static IObservable<Unit> WaitUntilAnimationEndsAsObservable(this Animator animator, int state,
-        int layerIndex = 0, float normalizedTimeOffset = 0f)
-    {
-        return animator.WaitUntilAnimationEnds(state, layerIndex, normalizedTimeOffset).ToObservable();
     }
 
     private static IEnumerator WaitUntilAnimationEnds(this Animator animator, int state, int layerIndex,
@@ -77,14 +53,5 @@ public static class AnimatorExtensions
         yield return null;
         yield return animator.WaitUntilAnimationEnds(animator.GetCurrentAnimatorStateInfo(layerIndex).fullPathHash,
             layerIndex, normalizedTimeOffset);
-    }
-
-
-    public static void SetTimeForAnimations(this Animation animation, float percent)
-    {
-        foreach (AnimationState animState in animation)
-        {
-            animState.normalizedTime = animState.length * percent;
-        }
     }
 }
